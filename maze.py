e3c4fb39f701abb0bcbea1bc58ba04c445c26a1d
Shreeya Kannan <shreeyakannan@gmail.com>
2024-11-03 20:59:53 +0530

    removed file path

--- maze.py
+++ maze.py
@@ -8,26 +8,25 @@ DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 class MazeApp:
-    def __init__(self, master, file_path, title, algorithm_func, cell_size=30):
+    def __init__(self, master, title, algorithm_func, cell_size=30):
         self.master = master
         self.master.title(title)  # Set window title
-        self.algorithm_func = algorithm_func  # Algorithm function (A* or Dijkstra)
+        self.algorithm_func = algorithm_func
         self.cell_size = cell_size
 
         # Load the maze from a text file
-        self.grid = self.load_maze(file_path)
-        self.rows = len(self.grid)
-        self.cols = len(self.grid[0])
+        self.rows = 20
+        self.cols = 20
 
         # Set up the canvas with the correct size
         self.canvas = tk.Canvas(master, width=self.cols * cell_size, height=self.rows * cell_size)
         self.canvas.pack()
 
+        # Find start and end points
+        self.start, self.end = (0, 0), (9, 9)
+
         # Draw the initial maze
         self.draw_maze()
-
-        # Find start and end points
-        self.start, self.end = self.find_start_and_end()
 
         # Run the algorithm and animate the solution
         path, visited_nodes = self.algorithm_func(self.start, self.end)
@@ -58,12 +57,6 @@ class MazeApp:
                     self.color_cell(neighbor[0], neighbor[1], 'red')
             # self.animate_path(path)
 
-    def load_maze(self, file_path):
-        """Load the maze from a text file."""
-        with open(file_path, 'r') as file:
-            maze = [list(line.strip()) for line in file.readlines()]  # Read as list of characters
-        return maze
-
     def draw_maze(self):
         """Draw the maze on the Tkinter canvas."""
         for r in range(self.rows):
@@ -76,7 +69,7 @@ class MazeApp:
                     color = 'red'  # Start point
                 elif self.grid[r][c] == 'E':
                     color = 'red'  # End point'''
-                if self.grid[r][c] == 'S' or self.grid[r][c] == 'E':
+                if (r, c) == self.start or (r, c) == self.end:
                     color = 'red'
                 else:
                     color = 'yellow'
@@ -102,34 +95,19 @@ class MazeApp:
             outline = 'black'
         self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=outline)
 
-    def find_start_and_end(self):
-        """Find the start (S) and end (E) points in the maze."""
-        start = None
-        end = None
-        for r in range(len(self.grid)):
-            for c in range(len(self.grid[0])):
-                if self.grid[r][c] == 'S':
-                    start = (r, c)
-                elif self.grid[r][c] == 'E':
-                    end = (r, c)
-        return start, end
-
 
 if __name__ == "__main__":
-    # Path to the text file containing the maze structure
-    maze_path = 'grid.txt'
-
     # First window for A* algorithm
     root_astar = tk.Tk()
-    astar_app = MazeApp(root_astar, maze_path, "A* Algorithm", astar.run_astar)
+    astar_app = MazeApp(root_astar, "A* Algorithm", astar.run_astar)
 
     # Second window for Dijkstra algorithm
     root_dijkstra = tk.Tk()
-    dijkstra_app = MazeApp(root_dijkstra, maze_path, "Dijkstra Algorithm", dijkstra.run_dijkstra)
+    dijkstra_app = MazeApp(root_dijkstra, "Dijkstra Algorithm", dijkstra.run_dijkstra)
 
     # Third window for Flood fill algorithm
     root_flood = tk.Tk()
-    flood_app = MazeApp(root_flood, maze_path, "Flood Fill Algorithm", flood.run_flood)
+    flood_app = MazeApp(root_flood, "Flood Fill Algorithm", flood.run_flood)
 
     # Start both Tkinter windows
     root_astar.mainloop()
